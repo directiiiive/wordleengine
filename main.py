080064045b9ddc48acf34db8ecaa8b216c8c8df3
directiiiive <86031533+directiiiive@users.noreply.github.com>
2022-02-14 23:37:53 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ class wordle():
         self.finaldict = {"no": [], "odd": [], "right": []}
         self.lastguess = "crane"
         self.wordlist = [n.rstrip() for n in file]
+        self.movesleft = 6
 
     def valuesparse(self,wordweird):
         for i in range(5):
@@ -16,8 +17,12 @@ class wordle():
                 self.finaldict["no"].append(currentl)
             if currentv == "o":
                 self.finaldict["odd"].append([currentl,i])
+                if currentl in self.finaldict["no"]:
+                    self.finaldict["no"].remove(currentl)
             if currentv == "r":
                 self.finaldict["right"].append([currentl,i])
+                if currentl in self.finaldict["no"]:
+                    self.finaldict["no"].remove(currentl)
 
         return (self.finaldict)
 
@@ -76,6 +81,7 @@ class wordle():
     def ranker(self):
         strong = 0
         strongest = ""
+        sum = 0
 
         def strength(word):
             letterdict = {"a": 2348, "b": 715, "c": 964, "d": 1181, "e": 3009, "f": 561, "g": 679, "h": 814, "i": 1592,
@@ -86,10 +92,44 @@ class wordle():
                 value += letterdict[i]/((word.count(i))**2)
             return value
 
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        letterstrength = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0,
+                          "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
+                          "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+        for i in alphabet:
+            for j in self.wordlist:
+                if j.find(i) != -1:
+                    letterstrength[i] += 1
+
+        def weirdstrength(word, letterstrength, goodletters):
+            weirdstrength = 0
+            letters = ""
+            for i in goodletters:
+                if goodletters.count(i) < len(self.wordlist):
+                    letters += i
+            for i in word:
+                if i in letters:
+                    weirdstrength += letterstrength[i]/word.count(i)
+            #print(word)
+            return weirdstrength
+
         for i in self.wordlist:
+            sum += strength(i)
             if strength(i) >= strong:
                 strongest = i
                 strong = strength(i)
+
+        print((strong - sum / len(self.wordlist)))
+        if (strong - sum / len(self.wordlist))/len(self.wordlist) < 200 and 2<len(self.wordlist)<5 and self.movesleft > 1:
+            goodletters = ""
+            for i in self.wordlist:
+                goodletters += i
+            strong = 0
+            f = open("words.txt", "r", encoding="utf-8")
+            for i in [line.rstrip() for line in f]:
+                if weirdstrength(i, letterstrength, goodletters) >= strong:
+                    strongest = i
+                    strong = weirdstrength(i, letterstrength, goodletters)
         self.lastguess = strongest
         return strongest
 
@@ -100,6 +140,9 @@ print("*instructions* the word it suggests will pop up above the 'result:' promp
       "green = 'r', yellow = 'o', grey = 'n'. based on that, it will give you another suggested word. keep doing this until the game ends.\n")
 while ask != "done":
     print(world.lastguess)
+    if world.lastguess in world.wordlist:
+        world.wordlist.remove(world.lastguess)
+    world.movesleft -= 1
     ask = input("result:\n")
     if len(ask) == 5:
         world.valuesparse(ask)
